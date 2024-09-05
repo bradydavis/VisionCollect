d30af3108da5a098a11a7cab8b8fd425de625cb5
brady davis <bradydavis@gmail.com>
2024-09-05 09:18:31 -0500

    Initial Commit

---
+++ VisionCollect/VisionCollectApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionCollectApp.swift
+//  VisionCollect
+//
+//  Created by Brady Davis on 9/5/24.
+//
+
+import SwiftUI
+
+@main
+struct VisionCollectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
